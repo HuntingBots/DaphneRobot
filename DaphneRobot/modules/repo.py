63d615b6f6bb19b02b39c9d545dc370374d14be9
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-05 10:09:28 +0530

    Update repo.py

--- DaphneRobot/modules/repo.py
+++ DaphneRobot/modules/repo.py
@@ -38,7 +38,7 @@ __HELP__ = (
 @capture_err
 async def repo(_, message):
     users = await get(
-        "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
+        "https://api.github.com/repos/HuntingBots/DaphneRobot/contributors"
     )
     list_of_users = ""
     count = 1
@@ -48,7 +48,7 @@ async def repo(_, message):
         )
         count += 1
 
-    text = f"""[Github](https://github.com/thehamkercat/WilliamButcherBot) | [Group](t.me/PatheticProgrammers)
+    text = f"""[Github](https://github.com/HuntingBots/DaphneRobot) | [Group](t.me/https://t.me/helpcenterbot1)
 ```----------------
 | Contributors |
 ----------------```
