247575df33fb75669a4f36ed2fbfd325039a9791
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-05 08:23:16 +0530

    Add files via upload

---
+++ DaphneRobot/core/keyboard.py
@@ -0,0 +1,42 @@
+"""
+MIT License
+
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from DaphneRobot.utils.functions import get_urls_from_text
+
+
+def btn(text, data, type=None):
+    type = "url" if get_urls_from_text(data) else "callback_data"
+    return InlineKeyboardButton(text, **{type: data})
+
+
+def ikb(keyboard: list):
+    lines = []
+    for row in keyboard:
+        line = []
+        for button in row:
+            button = btn(*button)  # InlineKeyboardButton
+            line.append(button)
+        lines.append(line)
+    return InlineKeyboardMarkup(inline_keyboard=lines)
